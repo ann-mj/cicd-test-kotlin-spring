30337935dcecc8561e4c4d6ea42288fb5cd53dfa
myeongjin.ann <myeongjin.ann@navercorp.com>
2024-05-01 20:51:39 +0900

    Init: Init project

---
+++ src/main/kotlin/com/example/cicdtestprj/CicdtestprjApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cicdtestprj
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CicdtestprjApplication
+
+fun main(args: Array<String>) {
+    runApplication<CicdtestprjApplication>(*args)
+}
